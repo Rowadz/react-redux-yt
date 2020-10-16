129dc5c386a2abe51383b67216a0765fe75b452d
MohammedAl-rowad <rowadwo@gmail.com>
2020-10-16 17:36:42 +0300

    Add redux devtool

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,10 +1,11 @@
 import { combineReducers, createStore } from 'redux'
 import { todos, addTodo, Todo } from './todos'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const reducers = combineReducers({
   todos,
 })
 
-export default createStore(reducers)
+export default createStore(reducers,  composeWithDevTools())
 
 export { addTodo, Todo }
