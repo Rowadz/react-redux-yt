8a90dc5a120d82dff7342f638a9f9e1f6cec0820
MohammedAl-rowad <rowadwo@gmail.com>
2020-10-16 15:07:45 +0300

    Simple TODO form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from 'react'
-import 'rsuite/dist/styles/rsuite-default.css'
-import { Button } from 'rsuite'
+import 'rsuite/dist/styles/rsuite-dark.css'
+import {
+  Button,
+  Container,
+  Header,
+  Navbar,
+  Content,
+  FlexboxGrid,
+  Panel,
+  Form,
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  ButtonToolbar,
+} from 'rsuite'
 import './App.css'
 import { connect } from 'react-redux'
 import { addTodo, Todo } from './redux'
@@ -12,11 +25,36 @@ const App = ({ dispatch, todos }) => {
   console.log(todos)
 
   return (
-    <main>
-      <Button appearance="primary" onClick={createTodo}>
-        Test
-      </Button>
-    </main>
+    <div className="main">
+      <Container>
+        <Header>
+          <Navbar appearance="inverse">
+            <Navbar.Header>
+              <p className="navbar-brand">Redux React TODO</p>
+            </Navbar.Header>
+          </Navbar>
+        </Header>
+        <Content>
+          <FlexboxGrid justify="center">
+            <FlexboxGrid.Item colspan={12}>
+              <Panel header={<h3>Add ToDo</h3>} bordered>
+                <Form fluid>
+                  <FormGroup>
+                    <ControlLabel>What you want to do?</ControlLabel>
+                    <FormControl name="task" />
+                  </FormGroup>
+                  <FormGroup>
+                    <ButtonToolbar>
+                      <Button appearance="primary">Create</Button>
+                    </ButtonToolbar>
+                  </FormGroup>
+                </Form>
+              </Panel>
+            </FlexboxGrid.Item>
+          </FlexboxGrid>
+        </Content>
+      </Container>
+    </div>
   )
 }
 
